b33e10fea5de7c46625e9c95c48413ba910cb8ff
YCRiver3038 <yuuki.cityriver@gmail.com>
2025-09-01 17:52:38 +0900

    helpコマンドの整備

--- src/main.cpp
+++ src/main.cpp
@@ -161,6 +161,23 @@ class GaplessLooper : public WaveFile {
         }
 };
 
+void showHelp() {
+    printf("args: --help, --list-devices, --loadonly --output-device, --chunklength, --rblength, --file, --noloop\n");
+    printf("--help                       : Show this help\n"
+           "--list-devices               : Show sound devices and exit.\n"
+           "--loadonly                   : Only load wave file (and exit without playing).\n"
+           "--output-device <index: int> : Set sound output device to device No.<index>.\n"
+           "                               index can be retrieved with function --list-devices.\n"
+           "--chunklength <length: int>  : Set chunk length to <length>.\n"
+           "                               chunk length is the sample length which application reads from file in once.\n"
+           "                               detail: chunklength = (number of sample) * (number of audio channel)\n"
+           "--rblength <length: int>     : Set ring buffer length to <length>.\n"
+           "                             : at least (chunklength * 2) shuld be set.\n"
+           "                             : application will retrieve double length by set length.\n"
+           "--file <filename: str>       : Set file name to load.\n"
+           "--noloop                     : Don't loop file if set.\n");
+}
+
 int main(int argc, char* argv[]) {
 #if defined(__linux__) || defined(__APPLE__)
     struct sigaction sa = {};
@@ -193,7 +210,7 @@ int main(int argc, char* argv[]) {
         getoptStatus = getopt_long(argc, argv, "", long_options, &optionIndex);
         switch (getoptStatus) {
             case 'h':
-                printf("args: --help, --list-devices, --loadonly --output-device, --chunklength, --rblength, --file\n");
+                showHelp();
                 return 0;
             case 1000: {
                 AudioManipulator initOnly;
